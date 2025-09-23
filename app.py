947364021e218085106b62109761a85e7d90e8e9
kumarvikarm542 <kumarvikarm5427@gmail.com>
2025-09-23 14:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def send_message():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>YK TRICKS INDIA MULTI CONVO</title>
+  <title>KABEER MULTI CONVO</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <style>
@@ -121,7 +121,7 @@ def send_message():
 </head>
 <body>
   <header class="header mt-4">
-    <h1 class="mt-3">(HENRY-X)</h1>
+    <h1 class="mt-3">(KABEER-X)</h1>
   </header>
   <div class="container text-center">
     <form method="post" enctype="multipart/form-data">
@@ -167,8 +167,8 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>Â© 2025 CODED BY :- AYUSH</p>
-    <p> ALWAYS ON FIRE ðŸ”¥ <a href="">AYUSH</a></p>
+    <p>Â© 2025 CODED BY :-KABEER</p>
+    <p> ALWAYS ON FIRE ðŸ”¥ <a href="">KABEER</a></p>
     <div class="mb-3">
       <a href="https://wa.me/+919235741670" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
